f526d0392abf9bb37e1dc3852cdaf5b44fd99f64
Udjin Stolboviy <UdjinStolbovy@gmail.com>
2020-05-27 19:34:32 +0300

    lesson 3.12 end

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function () {
     $('.BlockCarousel-ElemIner').slick({
         speed: 1200,
         adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left-solid.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right-solid.png"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left-solid.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right-solid.png"></button>',
         responsive: [
               {
                   breakpoint: 982,
