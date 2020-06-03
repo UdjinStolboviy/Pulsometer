caf76e302ceb505434ce6af48a7d9c4b76f4e202
Udjin Stolboviy <UdjinStolbovy@gmail.com>
2020-06-03 19:13:46 +0300

    lesson 3.15 time 27.46

--- src/js/script.js
+++ src/js/script.js
@@ -14,4 +14,10 @@ $(document).ready(function () {
               },
         ]
     });
+     $('ul.BlockCatalog-ElemTabs').on('click', 'li:not(.BlockCatalog-ElemTab_modActive)', function () {
+         $(this)
+             .addClass('BlockCatalog-ElemTab_modActive').siblings().removeClass('BlockCatalog-ElemTab_modActive')
+             .closest('div.BlockContainer').find('div.BlockCatalog-ElemContent').removeClass('BlockCatalog-ElemContent_modActive').eq($(this).index()).addClass('BlockCatalog-ElemContent_modActive');
+     });
+
 });
